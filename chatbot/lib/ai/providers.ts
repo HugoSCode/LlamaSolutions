cc11c2c17c8cd232be53c9bbdaa50fbf03b8803b
HugoSCode <smitht@student.op.ac.nz>
2026-09-23 15:44:56 +1200

    Changed baseurl for test

--- chatbot/lib/ai/providers.ts
+++ chatbot/lib/ai/providers.ts
@@ -5,8 +5,9 @@ import { isTestEnvironment } from "../constants";
 
 import { titleModel } from "./models";
 
+
 const lmstudio = createOpenAI({
-  baseURL: `${process.env.LMSTUDIO_BASE_URL}/v1` || "http://localhost:1234/v1",
+  baseURL: process.env.LMSTUDIO_BASE_URL || "http://localhost:1234/v1",
   apiKey: process.env.LMSTUDIO_API_KEY || "lm-studio",
   headers: {
     'ngrok-skip-browser-warning': 'true',
